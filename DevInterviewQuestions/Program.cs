f37f81b49d5acbf0db5d1377fd244a732492fc53
gaffneym13 <45971900+gaffneym13@users.noreply.github.com>
2022-03-24 18:32:19 +0000

    Add project files.

---
+++ DevInterviewQuestions/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DevInterviewQuestions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string reversedString = ReverseString("Hello");
+
+            Console.WriteLine(reversedString);
+
+            Console.ReadLine();
+        }
+
+
+        public static string ReverseString(string input)
+        {
+            int strLength = input.Length;
+
+            string output = new string("");
+
+            for (int i = strLength-1; i > -1; i--)
+            {
+                output = string.Concat(output, input[i]);
+            }
+
+            return output;
+        }
+    }
+}
