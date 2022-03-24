454126ba421e37e574cd030875d70d18460faffb
gaffneym13 <45971900+gaffneym13@users.noreply.github.com>
2022-03-24 18:52:40 +0000

    Started question 2

--- DevInterviewQuestions/Program.cs
+++ DevInterviewQuestions/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DevInterviewQuestions
 {
@@ -9,6 +10,11 @@ namespace DevInterviewQuestions
             string reversedString = ReverseString("Hello");
 
             Console.WriteLine(reversedString);
+
+            var input = new int[][] { new[] { 1, 1, 5, 8 }, new[] { 1, 0, 9, 6 }, new[] { 4, 5, 7, 9 } };
+            var output = new int[][] { new[] { 1, 0, 5, 8 }, new[] { 0, 0, 0, 0 }, new[] { 4, 0, 7, 9 } };
+
+
 
             Console.ReadLine();
         }
@@ -26,6 +32,30 @@ namespace DevInterviewQuestions
             }
 
             return output;
+        }
+
+        public int[][] Answer(int[][] input)
+        {
+            var rows = input.Length;
+            var columns = input[0].Length;
+
+            var zeroCoordinates = new List<int[]>();
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (input[i][j] == 0)
+                    {
+                        zeroCoordinates.Add(new[] { i, j });
+                    }
+                }
+            }
+
+            foreach (int[] coordinate in zeroCoordinates)
+            {
+
+            }
         }
     }
 }
